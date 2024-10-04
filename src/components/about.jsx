9916ff76526046bc126cbce86566086517d40d95
ArpitaPanda261 <arpitapanda261@gmail.com>
2024-10-04 11:29:41 +0530

    Changed images

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,18 +6,18 @@ export const About = (props) => {
     <div id="about">
       <div className="container">
         <div className="row">
-          <div className="col-xs-12 col-md-6">
-            {" "}
-            <AnimatedComponent animationType="scaleUp" >
-            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+          <div className="col-12 col-md-6">
+            <AnimatedComponent animationType="scaleUp">
+              <img src="img/about.jpg" className="img-fluid" alt="About Us" />
             </AnimatedComponent>
           </div>
-          <div className="col-xs-12 col-md-6">
+          <div className="col-12 col-md-6">
             <div className="about-text">
-            <AnimatedComponent animationType="scaleUp" >
-              <h2>About Us</h2>
+              <AnimatedComponent animationType="scaleUp">
+                <h2>About Us</h2>
               </AnimatedComponent>
               <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              {/* Keep this commented section as is */}
               {/* <h3>Why Choose Us?</h3>
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
